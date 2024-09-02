79210d13900681b1d2cf1f3c146efbf9af12f378
Casmel <sacolorado@unal.edu.co>
2024-09-02 11:56:18 -0500

    Update utilidad.py

--- src/uiMain/utilidad.py
+++ src/uiMain/utilidad.py
@@ -1,3 +1,5 @@
+from gestorAplicacion.Entorno.ciudad import Ciudad
+
 class Utilidad:
 
     def readInt():
@@ -7,4 +9,29 @@ class Utilidad:
         except:
             print("Ingrese un número entero.")
             Utilidad.readInt()
+    
+    def readFloat():
+        try:
+            num = float(input("Ingrese el número: "))
+            return num
+        except:
+            print("Ingrese un número entero.")
+            Utilidad.readFloat()
 
+    def listadoCiudades():
+        if len(Ciudad.get_ciudades) != 0:
+            Ciudad.get_ciudades().sort(key = lambda ciudad: ciudad.get_nombre(), reverse = True)
+            for i in range(0, len(Ciudad.get_ciudades())):
+                print(f"{i + 1}. {Ciudad.get_ciudades[i].get_nombre()}")
+        else: 
+            pass
+
+    def listadoZonasCiudad(ciudad = Ciudad()):
+        if len(ciudad.get_zonas_ciudad()) != 0:
+            ciudad.get_zonas_ciudad().sort(key = lambda zona: zona.get_nombre(), reverse = True)
+            for i in range(0, len(ciudad.get_zonas_ciudad())):
+                print(f"{i + 1}. {ciudad.get_zonas_ciudad()[i].get_nombre()}")
+        else:
+            pass
+
+#Es posible que ni siquiera se usen estos métodos, así que de ser necesarios, los traduzco.
