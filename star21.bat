52afd9371d0d4c3b83c64457e4b8290cd6b82add
juanjoseArangom <jarangoma@unal.edu.co>
2024-09-17 12:17:34 -0500

    funcionando

--- star21.bat
+++ star21.bat
@@ -4,9 +4,6 @@ cd /d "%~dp0"
 :: Instalar Pillow si no está instalado (solo necesario la primera vez)
 pip install Pillow
 
-:: Crear el ejecutable usando pyinstaller. Ajusta la ruta al archivo main.py
-pyinstaller --onefile --windowed --add-data "src/uiMain/Imagenes;src/uiMain/Imagenes" src/main.py
-
 :: Ejecutar el script directamente usando la ruta correcta
 python src/main.py
 
